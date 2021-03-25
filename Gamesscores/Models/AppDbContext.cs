c99272bfccb94bbe6253286eff2eaf78d0ec3deb
SaiidZafari <saiid.zafari@bahnhof.se>
2021-03-25 06:52:04 +0100

    Add AppDbContext

---
+++ Gamesscores/Models/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gamesscores.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        DbSet<Highscore> Highscores { get; set; }
+    }
+}
---
+++ Gamesscores/Models/Highscore.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gamesscores.Models
+{
+    public class Highscore
+    {
+        public int Id { get; set; }
+
+        public int Score { get; set; }
+
+        public string Fullname { get; set; }
+
+        public string Game { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
